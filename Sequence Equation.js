f6573f8af7faf1ef27b45ed07744d74202d76025
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-11-25 23:12:19 +0530

    misc

--- Sequence Equation.js
+++ Sequence Equation.js
@@ -1,34 +1,4 @@
 // https://www.hackerrank.com/contests/mountblue-technologies/challenges/permutation-equation
-'use strict';
-
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the 'permutationEquation' function below.
- *
- * The function is expected to return an INTEGER_ARRAY.
- * The function accepts INTEGER_ARRAY p as parameter.
- */
 
 function permutationEquation(p) {
     // Write your code here
@@ -39,18 +9,4 @@ for(let i=1;i<=p.length;i++){
     result.push(secondIndex)
 }
 return result
-}
-
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const n = parseInt(readLine().trim(), 10);
-
-    const p = readLine().replace(/\s+$/g, '').split(' ').map(pTemp => parseInt(pTemp, 10));
-
-    const result = permutationEquation(p);
-
-    ws.write(result.join('\n') + '\n');
-
-    ws.end();
 }
